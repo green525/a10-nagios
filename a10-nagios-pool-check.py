878e92711d77ffe2f28d1884239b7546b9c0ff50
Darren Gibbard <dalgibbard@gmail.com>
2014-03-19 15:06:05 +0000

    Added initial code, usage, do_exit and list functions etc.

--- a10-nagios-pool-check.py
+++ a10-nagios-pool-check.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python
-import getopt, sys, functools
-import json, urllib, urllib2
-import httplib, ssl
 
+## Import the required modules for our code
+import getopt, sys, functools, urllib, urllib2, httplib, ssl, json
+
+## Redefine the SSL Socket code in the ssl module to use TVSv1 by default,
+## as the old default of v1.1 is unsupported by the A10.
 old_init = ssl.SSLSocket.__init__
 
 @functools.wraps(old_init)
-def ubuntu_openssl_bug_965371(self, *args, **kwargs):
+def new_ssl_fix(self, *args, **kwargs):
   kwargs['ssl_version'] = ssl.PROTOCOL_TLSv1
   old_init(self, *args, **kwargs)
 
-ssl.SSLSocket.__init__ = ubuntu_openssl_bug_965371
+## Remap our new SSL code as an in-place replacement to the old code.
+ssl.SSLSocket.__init__ = new_ssl_fix
 
+## Define a 'Usage' page which displays details on how to use this code.
 def usage():
     print("")
-    print("Usage: %s -u <username> -p <password> -h <loadbalancer> [-l] [-v <vrrid>] [-c <PERCENT>] [-w <PERCENT>] [-n <NAME>]" % sys.argv[0])
+    print("Usage: %s -u <username> -p <password> -h <loadbalancer> [-l] [-g <slb_group_id>] [-c <PERCENT>] [-w <PERCENT>] [-n <NAME>]" % sys.argv[0])
     print("")
-    print("-u <username>		=> Username to authenticate against the A10")
-    print("-p <password>		=> Password to authenticate against the A10")
-    print("-h <loadbalancer>	=> A10's Hostname or IP to Connect to")
-    print("-l			=> List available VRRIDS")
-    print("-v <vrrid>		=> Report on statistics for a single VRRID")
-    print("-c <PERCENTAGE>		=> Percentage of remaining active nodes to report Critical Status on")
-    print("-w <PERCENTAGE>		=> Percentage of remaining active node to report Warning Status on")
-    print("-n <NAME>		=> Friendly name for the VRRID for alerting.")
+    print("    -u <username>		=> Username to authenticate against the A10")
+    print("    -p <password>		=> Password to authenticate against the A10")
+    print("    -h <loadbalancer>	=> A10's Hostname or IP to Connect to")
+    print("    -l			=> List available VRRIDS")
+    print("    -g <slb_service_group_ID>")
+    print("				=> Report on statistics for a single SLB Service Group")
+    print("    -c <PERCENTAGE>		=> Percentage of remaining active nodes to report Critical Status on")
+    print("    -w <PERCENTAGE>		=> Percentage of remaining active node to report Warning Status on")
+    print("    -n <NAME>		=> Friendly name for the VRRID for alerting.")
+    print("    -d			=> Enable Debug")
     print("")
     sys.exit(1)
 
-# Parse args
+## Parse the arguments a user has given us
 try:
-    myopts, args = getopt.getopt(sys.argv[1:],"u:p:h:lv:c:w:n:")
+    myopts, args = getopt.getopt(sys.argv[1:],"u:p:h:lg:c:w:n:d")
 except getopt.GetoptError:
     usage()
 
+## Set some initial values for some of our variables, before we potentially
+## overwrite them with user options
 listonly = False
-user, password, loadbalancer, vrrid = "", "", "", ""
+user, password, loadbalancer, slbgroupid = "", "", "", ""
 warn = 80
 crit = 50
+debug = False
 
+## Iterate over each argument provided by the user, and try to map each one to
+## a particular setting or variable we need.
 for o, a in myopts:
     if o == '-u':
         user = str(a)
@@ -47,52 +58,83 @@ for o, a in myopts:
         loadbalancer = str(a)
     elif o == '-l':
         listonly = True
-    elif o == '-v':
-	vrrid = str(a)
+    elif o == '-g':
+	slbgroupid = str(a)
     elif o == '-c':
         crit = str(a)
     elif o == '-w':
         warn = str(a)
     elif o == '-n':
         name = str(a)
+    elif o == '-d':
+        debug = True
     else:
-        print("Unknown options: " . str(o))
+        print("Unknown option: " . str(o))
         print("")
         usage()
 
-for x in user, password, loadbalancer, vrrid:
+## Now we've checked the user's arguments, make sure they've at least given us the basics.
+for x in user, password, loadbalancer:
     if x == "":
         usage()
 
-## Original Code
+## Validate that at EITHER 'slbgroupid' OR 'listonly' have been requested - but not none, or both.
+if slbgroupid == "" and listonly == False:
+    usage()
+if slbgroupid != "" and listonly == True:
+    usage()
+
+## do_exit  -- Used for exiting in a 'Nagios Compliant' way later:
+def do_exit(state, msg):
+    if state == 0:
+        condition = "OK"
+    elif state == 1:
+        condition = "WARNING"
+    elif state == 2:
+        condition = "CRITICAL"
+    else:
+        # Assume state = 3
+        condition = "UNKNOWN"
+
+    print("%s: %s" % (condition, msg))
+    sys.exit(state)
+
+
+## AUTHENTICATE AGAINST THE LB TO GET A SESSION ID
 c = httplib.HTTPSConnection(loadbalancer)
 sessionurl = "/services/rest/V2/?method=authenticate&username=%s&password=%s&format=json" % (user, password)
 c.request("GET", sessionurl)
 response = c.getresponse()
 data = json.loads(response.read())
+# Try and assign the session_id variable from what was returned to us from the LB.
 try:
     session_id = data['session_id']
-    print "Session Created. Session ID: " + session_id
+    if debug == True:
+        print "Session Created. Session ID: " + session_id
+# If the data isn't there for assigning (ie. Connection or Auth error) we'll get a KeyError instead.
 except KeyError:
-    print("Failed Authorization")
+    errmsg = "Failed to connect/auth against Loadbalancer %s" % loadbalancer
+    do_exit(3, errmsg)
 
+## If 'listonly' is defined as True, we'll just list the available 'slb.service_group' data, and exit
+## NOTE: This is only for initial use when trying to work out which SLB GID's you want to monitor-
+## Therefore, this doesn't provide nice output really!
+if listonly == True:
+    c = httplib.HTTPSConnection(loadbalancer)
+    sessionurl = "/services/rest/V2/?session_id=" + session_id + "&method=&format=json&method=slb.service_group.getAll"
+    c.request("GET", sessionurl)
+    response = c.getresponse()
+    data = json.loads(response.read())
+    try:
+        response = data['response']
+        status = str(response['status'])
+        message = str(response['err']['msg'])
+        if status == "fail":
+            print("Failed to obtain data. Error was: %s - %s" % (status, message))
+            sys.exit(1)
+        print(response)
+    except KeyError:
+        print("Failed to connect/auth against Loadbalancer %s" % loadbalancer)
+        sys.exit(1)
 
-#  Step 1. Create Servers     #
-###############################
-# Create slb servers s1 10.0.2.128
-# Construct HTTP URL and Post Body
-#post_body = json.dumps(
-##{"server":
-#{
-#"name": "s1",
-#"host": "10.0.2.128",
-#"health_monitor": "(default)",
-#"port_list": [
-#{"port_num": 80,
-#"protocol": 2,}
-#],
-#}
-#}
-#)
-#url = "https://172.31.31.121/services/rest/V2/?&session_id=" + session_id + "&format=json&method=slb.server.create"
-#print "URL Created. URL: " + url + " body: " + post_body
+## Now for the real code: ...
